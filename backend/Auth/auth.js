fc8c9e34278989270c33eaaa2a7569d72a26f07d
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2025-04-30 15:08:40 +0530

    removed console.logs

--- backend/Auth/auth.js
+++ backend/Auth/auth.js
@@ -4,22 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
-console.log("JWT_SECRET:", JWT_SECRET);
 export const authenticated = async (req, res, next) => {
   try {
-    console.log(req.cookies);
     const token = req.cookies.authToken;
-    console.log("Token received:", token);
     if (!token) {
-      console.log("user unauthorised");
       return res.status(401).json({ message: "Unauthorized" });
     }
     const decode = jwt.verify(token, JWT_SECRET);
-  console.log(decode)
     const user = await User.findById(decode.id);
-    console.log(user)
     if (!user) {
-      console.log("user unauthorised");
       return res.status(401).json({ message: "Unauthorized" });
     }
 
