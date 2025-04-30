587894064345c594e42668fb39681c01bc0df57c
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2025-04-30 14:15:58 +0530

    cors changed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
-    "https://ticketing-system-app-1-obz1yauni-lokeshs-projects-f594ae13.vercel.app",
+    "https://ticketing-system-app-1-obz1yauni-lokeshs-projects-f594ae13.vercel.app/",
     "http://localhost:5174",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
