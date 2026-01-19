1bd9eada21b86f8f808a35da6d23bbf30746afd7
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2026-01-19 11:11:59 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
-    "https://ticketing-system-dusky.vercel.app/",
-    "https://ticketing-system-lokeshs-projects-f594ae13.vercel.app/",
-    "https://ticketing-system-git-main-lokeshs-projects-f594ae13.vercel.app/",
     "https://ticketing-system-5x7cm3b5a-lokeshs-projects-f594ae13.vercel.app/",
-    "http://localhost:5173",
+    "https://ticketing-system-git-main-lokeshs-projects-f594ae13.vercel.app/",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
